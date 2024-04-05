30994e291c4563600b56ba98fd642f07d4fb2c80
CoderBak <hx-sun@outlook.com>
2024-04-05 15:30:49 +0800

    A demo of socket connection

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,31 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+        : QMainWindow(parent), ui(new Ui::MainWindow), socket(new QTcpSocket(this)) {
     ui->setupUi(this);
+    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::readData);
+    socket->connectToHost("localhost", 1234);
+    if (!socket->waitForConnected()) {
+        qDebug() << "Failed to connect to host.";
+        exit(1);
+    } else {
+        qDebug() << "Connected successfully!";
+    }
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked() {
+    QString number = ui->lineEdit->text();
+    qDebug() << number;
+    socket->write(number.toUtf8());
+}
+
+void MainWindow::readData() {
+    QByteArray data = socket->readAll();
+    ui->label->setText(data);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,28 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef CLIENT_H
+#define CLIENT_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
+namespace Ui { class MainWindow; }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow() override;
+
+private slots:
+
+    void on_pushButton_clicked();
+
+    void readData();
 
 private:
     Ui::MainWindow *ui;
+    QTcpSocket *socket;
 };
-#endif // MAINWINDOW_H
+
+#endif // CLIENT_H
